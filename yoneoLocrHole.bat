2f922c783f60ff4cc7076202fc3c8cd93faedc3c
YonekuraLab <42053473+YonekuraLab@users.noreply.github.com>
2021-04-08 12:28:51 +0900

    Delete yoneoLocrHole.bat

--- yoneoLocrHole.bat
+++
@@ -1,6 +0,0 @@
-call C:\Anaconda3\Scripts\activate.bat yolov5-4.0
-REM call C:\ProgramData\Miniconda3\Scripts\activate.bat yolov5-4.0
-
-python .\yoneoLocrWatch-yolov5.py --weights weights\holes800_210222.pt --object hole --device 0
-
-
