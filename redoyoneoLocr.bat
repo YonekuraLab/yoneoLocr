3f76332968f76bbf52e245ec1dceb09ca314ce4f
YonekuraLab <42053473+YonekuraLab@users.noreply.github.com>
2021-04-30 23:05:16 +0900

    Add files via upload

--- redoyoneoLocr.bat
+++ redoyoneoLocr.bat
@@ -3,7 +3,7 @@ set watchdir="WatchLowmagXtal"
 for %%i in (%1*.jpg) do (
 copy %%i %exepath%
 echo %%~ni.jpg %2 > %exepath%\%watchdir%\InputImage.txt
-timeout /t 1
+timeout /t 2
 move %exepath%\%%~ni.log %%~di%%~pi%%~ni.mrc.log
 del  %exepath%\%%~ni.jpg
 )
