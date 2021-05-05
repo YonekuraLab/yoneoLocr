0e4e0f7cc56a1cd7a4d05d80eacc32f03b9a4318
YonekuraLab <42053473+YonekuraLab@users.noreply.github.com>
2021-05-05 15:19:23 +0900

    Add files via upload

--- XF416/yoneoLowmagXtal.bat
+++ XF416/yoneoLowmagXtal.bat
@@ -8,7 +8,7 @@ REM python .\yoneoLocrWatch-yolov5.py --weights weights\lowmagxtals800_210325.pt
 if "%1"=="" ( set thres="0.5" ) else (set thres="%1" )
 C:
 cd \ProgramData\yoneoLocr\
-python .\yoneoLocrWatch-yolov5.py --weights weights\lowmagxtals800_210325.pt --object lowmagxtal --conf-thres %thres% --conf-sel 0.25 --device 0
+python .\yoneoLocrWatch-yolov5.py --weights weights\lowmagxtalx800_210505.pt --object lowmagxtal --conf-sel %thres% --device 0
 
 
 
